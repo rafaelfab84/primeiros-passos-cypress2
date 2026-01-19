4e0b289f81aaab9005e33fae213e2e57c08b9fa3
Rafael F. De Andrade <rafaelfab84@gmail.com>
2026-01-19 17:14:20 +0100

    primeiro commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,19 @@
+describe('template spec', () => {
+  it('login - success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Admin')
+    cy.get('[name="password"]').type('admin123')
+    cy.get('[type="submit"]').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+
+    it('login - fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('teste')
+    cy.get('[name="password"]').type('teste')
+    cy.get('[type="submit"]').click()
+    cy.get('.oxd-alert')
+  })
+
+})
