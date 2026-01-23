8ad791f5c05a0044fb5bc8521aa7f89de90d4d60
Rafael F. De Andrade <rafaelfab84@gmail.com>
2026-01-23 16:34:23 +0100

    added baseURL

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -14,7 +14,7 @@ describe('template spec', () => {
  
   
   it('login - success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorslist.usernameField).type(userData.userSuccess.username)
     cy.get(selectorslist.passwordField).type(userData.userSuccess.password)
     cy.get(selectorslist.loginButton).click()
@@ -24,7 +24,7 @@ describe('template spec', () => {
 
 
     it('login - fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('auth/login')
     cy.get(selectorslist.usernameField).type(userData.userFail.username)
     cy.get(selectorslist.passwordField).type(userData.userFail.password)
     cy.get(selectorslist.loginButton).click()
